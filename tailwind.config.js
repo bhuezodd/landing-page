e9a8d695b1467be0dfe698ee4ed795b056cb978a
Boris Huezo <huezo.dd@gmail.com>
2020-07-29 23:04:38 -0600

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,33 @@
 module.exports = {
   purge: [],
   theme: {
+    fontFamily: {
+      'sans': ["noto"],
+    },
+    textColor: {
+      'black': '#000000',
+      'white': '#FFFFFF',
+      'yellow': '#FFD600',
+      'gray': {
+        100: '#00000066',
+        200: '#78909C',
+        300: '#7B7B7B'
+      },
+    },
+    backgroundColor: {
+      'black': '#000000',
+      'white': '#FFFFFF',
+      'yellow': '#FFD600',
+      'test': '#000',
+      'gray': {
+        100: '#00000066',
+        200: '#78909C',
+        300: '#7B7B7B'
+      },
+    },
     extend: {},
   },
   variants: {},
   plugins: [],
 
-  fontFamily: {
-    'sans': ["noto"],
-  },
-  textColor: {
-    'black': '#000000',
-    'white': '#FFFFFF',
-    'yellow': '#FFD600',
-    'gray': {
-      100: '#00000066',
-      200: '#78909C',
-      300: '#7B7B7B'
-    },
-  },
-  backgroundColor: {
-    'black': '#000000',
-    'white': '#FFFFFF',
-    'yellow': '#FFD600',
-    'test': '#000',
-    'gray': {
-      100: '#00000066',
-      200: '#78909C',
-      300: '#7B7B7B'
-    },
-  }
 }
